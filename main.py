b52ef248589e03bdc2ea69b3d947fee3d4ff76a7
oluml <oleuml@users.noreply.github.com>
2020-10-09 16:01:14 +0200

    Refactoring of main.py

--- main.py
+++ main.py
@@ -16,16 +16,7 @@ def main(schedule_name, tournament_dict):
                       tournament_data=data)
     ui.mainloop()
 
-if __name__ == "__main__":
-    player_keys = ["Rafael DÜS", "Moggi DUI", "Ben MÜ","Patrick BRÜ", "Frank HAN",
-    "Aislan GRO", "Markus HAN", "Julius HAL", "Holger ALA", "Thommy HAN", "Aljoscha BER"]# "Christoph LEI", "Markus BER", "Richie LEI", "Emílio D. C. BER",
-    #"Max CAM"]
-    # "Florian GRO", "Levin HAL", "John MAN", "Oli MAN",
-    #"Dima Kharkiv", "Megan AUS", "Daniel HAL", "Judith HH ", "Antonio LON", "Domi ULM",
-    #"Sam ZÜR", "Thomas HAN ", "Rado FRA", "David MÜ", "Chris HH ", "Steffn HH",
-    #"Fabian BÜ", "Ole BÜ", "Flo BÜ", "Johannes BÜ", "Buby BÜ", "Kai BÜ", "Andy BÜ",
-    #"Franky BÜ"]
-    
+if __name__ == "__main__":    
     load_ui = LoadUI(".tournaments.json", command=main)
     load_ui.mainloop()
   
